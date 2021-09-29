6c126346da57bd3330122baa044d7c1006b7e59a
suraj60081 <sk9718680309@gmail.com>
2021-09-29 14:37:58 +0530

    String Anagrams

---
+++ Python/Anagrams.py
@@ -0,0 +1,8 @@
+s1 = "hello"
+s2 = "eholl"
+s3 = "oellh"
+
+if(sorted(s1)== sorted(s2) and sorted(s2)==sorted(s3)):
+        print("The strings are anagrams.")
+else:
+     print("The strings aren't anagrams.")      
